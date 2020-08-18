69cf201c383e0e589406541f0a01888972de6714
Saifur Rahman <saifur703@gmail.com>
2020-08-18 10:02:29 +0600

    Create Comment fields Order.php

---
+++ Comment fields Order.php
@@ -0,0 +1,29 @@
+
+//Comment fields Order
+// functions.php
+
+function halim_comment_filed( $fields ) {
+    $comment = $fields['comment'];
+    $author = $fields['author'];
+    $email = $fields['email'];
+    $url = $fields['url'];
+    $cookies = $fields['cookies'];
+
+    unset( $fields['comment'] );
+    unset( $fields['author'] );
+    unset( $fields['email'] );
+    unset( $fields['url'] );
+    unset( $fields['cookies'] );
+    
+    $fields['author'] = $author;
+    $fields['email'] = $email;
+    $fields['url'] = $url;
+    $fields['comment'] = $comment;
+    $fields['cookies'] = $cookies;
+    
+    return $fields;
+}
+add_filter( 'comment_form_fields', 'halim_comment_filed' );
+
+
+
